23bc70bdc6f912c48d8856ff908aafdc57456379
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2024-02-23 19:08:43 -0600

    fix:error correction

--- linkedList.js
+++ linkedList.js
@@ -1,41 +1,81 @@
-class node {
+class Node {
     constructor(data) {
-        this.data = data
-        this.next = data
+        this.data = data;
+        this.next = null;
     }
-
 }
-class linkdList {
+
+class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
     }
 
-    append(data) {
-        if (!data) return "No hay dato";
-        const newNode = new Node(data)
+    append(element) {
+        if (!element) {
+            return "There´s no data";
+        }
+
+        const node = new Node(element);
+
         if (!this.head) {
-            this.head = newNode;
+            this.head = node;
+            this.tail = node;
         } else {
-            this.tail.next = new Node;
-            this.tail = new Node;
+            this.tail.next = node;
+            this.tail = node;
         }
     }
 
-    prepend(data) {
-        if (!data) return "No hay dato";
-        const newNode = new Node(data);
+    prepend(element) {
+        if (!element) {
+            return "There´s no data";
+        }
+
+        const node = new Node(element);
 
         if (!this.head) {
-            this.head = new Node;
-            this.tail = new Node;
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
         }
+    }
+
+    insertAfterNode(node, element) {
+        if (!node || !element) {
+            return "Node and element are mandatory";
+        }
+
+        const newNode = new Node(element);
+
+        let current = this.head;
+        while (current) {
+            if (current.data === node) {
+                newNode.next = current.next;
+                current.next = newNode;
+                if (current === this.tail) {
+                    this.tail = newNode;
+                    return 'The element was appended and it is the tail';
+                };
+                return 'The element was appended';
+            }
+            current = current.next;
+        }
+        return 'The node doesn´t exist'
     }
 
     traverse() {
-        while (current) {
-            console.log(current.data);
-            current = current.next;
+        if (!this.head) {
+            return "There´s no data";
+        }
+
+        let currentValue = this.head;
+
+        while (currentValue) {
+            console.log(currentValue);
+            currentValue = currentValue.next;
         }
     }
 
@@ -65,35 +105,77 @@ class linkdList {
         return "Elemento no encontrado";
     }
 
-    deleteHead() {
+    deleteHead(){
         if (!this.head) {
-            return "No hay"
+            return 'There´s no head';
         }
-        this.head = this.head.next
+
+        this.head = this.head.next;
 
         if (this.head === null) {
-            this.tail = null
+            this.tail=null;
         }
-
     }
 
-    deleteTail() {
-        if (!this.head) return "No hay lista"
+    deleteTail(){
+        if (!this.head) {
+            return 'There´s no data';
+        }
+
         if (this.head === this.tail) {
             this.head = null;
             this.tail = null;
+            return 'The tail was removed and it was also head'
         }
+
         let current = this.head;
-        while (current.next) {
-            if (current.next === this.tail) {
-                this.tail = current
-                return "La cola ha sido reasignada"
+
+        while(current.next){
+            if(current.next ===  this.tail){
+                this.tail = current;
+                return 'The tail was removed';
             }
-            current = current.next
+            current = current.next;
         }
-
-
-
     }
-
 }
+
+const linkedList = new LinkedList();
+/*
+console.log("Lista ligada:");
+
+    console.time();
+    linkedList.prepend(5);
+    console.timeEnd();
+
+    console.time();
+    linkedList.append(15);
+    console.timeEnd();
+
+    console.time();
+    linkedList.deleteNode(15);
+    console.timeEnd();
+
+const numbers = [1, 2, 3, 4, 5];
+console.log("Arreglo: ");
+
+    console.time();
+    numbers.unshift(0);
+    console.timeEnd();
+
+    console.time();
+    numbers.push(6);
+    console.timeEnd();
+
+    console.time();
+    numbers.splice(2, 1);
+    console.timeEnd();
+
+console.log(numbers);
+*/
+
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+console.log(linkedList.insertAfterNode(10, 3));
+console.log(linkedList);
